d7563186519d369e10d8490827688ba069434bb5
Tom Waugh <thomas.w@day8.com>
2020-03-08 16:21:50 +0000

    Added missing NOT NULL to init.sql

--- db/init.sql
+++ db/init.sql
@@ -4,7 +4,7 @@ CREATE TABLE movies (
   genre VARCHAR(255) NOT NULL,
   lead_studio VARCHAR(255) NOT NULL,
   audience_score SMALLINT NOT NULL,
-  profitability NUMERIC(12, 9),
+  profitability NUMERIC(12, 9) NOT NULL,
   rotten_score SMALLINT NOT NULL,
   world_gross NUMERIC(6, 2) NOT NULL,
   year SMALLINT NOT NULL
