646d160bb578f4b2344a2b59a0e9d1c0cf18bc05
Tom Waugh <thomas.w@day8.com>
2020-03-08 15:19:38 +0000

    Implemented route and db query to get movies (optionally by string matching or sorting by scores)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { pool } = require('../db/db_config');
+
+app.get('/api/v1/suggestions', (req, res) => {
+  const { q, sortBy } = req.query;
+
+  if (sortBy && !['rotten', 'audience'].includes(sortBy)) {
+    res.status(400).json({ error: 'Invalid sortBy value. Valid values are \'audience\' and \'rotten\'.' }).end();
+    return;
+  }
+
+  let query = 'SELECT * FROM movies';
+  let values = [];
+
+  if (q) {
+    query += ' WHERE film ILIKE $1'; // ILIKE handles case
+    values.push(`%${q}%`);
+  }
+
+  // node-postgres doesn't handle parameterising that well but we only need to handle 2 cases so can use ifs
+  if (sortBy === 'audience') {
+    query += ' ORDER BY audience_score DESC';
+  } else if (sortBy === 'rotten') {
+    query += ' ORDER BY rotten_score DESC';
+  }
+
+  pool.query(
+    query,
+    values,
+    (error, results) => {
+      if (error) {
+        console.error(error);
+      }
+      res.status(200).json(results.rows)
+    }
+  );
+});
 
 app.get('*', (req, res) => res.send('Invalid route. Please check the documentation for help on using this api.'));
 
